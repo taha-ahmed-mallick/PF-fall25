6d2e2d66fb866c59f7b0b2722793547a7645c3d8
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-10-18 09:01:08 -0700

    Made some basics of CCP

--- CCP/main.c
+++ CCP/main.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
-#include <windows.h> 
+#include <windows.h>
+#include <stdlib.h>
 
-int main() {
+char board[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+void printBoard();
+int main()
+{
     SetConsoleOutputCP(CP_UTF8);
-    printf("\n o │ x │ o\n───┼───┼───\n x │ x │ o\n───┼───┼───\n o │ x │ x\n");
+    int player = 1, box, flag = 0;
+    char mark = 'X';
+    while (1)
+    {
+        system("cls");
+        printf("%d", board[0]);
+        printBoard();
+        mark = player == 1 ? 'X' : 'O';
+        if (flag)
+            printf("Invalid input by Player %d\n\tEnter again.", player);
+        printf("\nPlayer %d [%c], enter position (1-9): ", player, mark);
+        scanf("%d", &box);
+        if (box < 1 || box > 9 || board[box - 1] != box + '0')
+        {
+            flag = 1;
+            continue;
+        }
+        flag = 0;
+        player = player == 1 ? 2 : 1;
+    }
     return 0;
 }
 
+void printBoard()
+{
+    printf("\t┌────────────────┐\n");
+    printf("\t│TIC TAC TOE GAME│\n");
+    printf("\t└────────────────┘\n");
+    printf("\n\t    %c │ %c │ %c", board[0], board[1], board[2]);
+    printf("\n\t   ───┼───┼───");
+    printf("\n\t    %c │ %c │ %c", board[3], board[4], board[5]);
+    printf("\n\t   ───┼───┼───");
+    printf("\n\t    %c │ %c │ %c\n", board[6], board[7], board[8]);
+}
+
 /*
- o │ x │ o 
+ o │ x │ o
 ───┼───┼───
- x │ x │ o 
+ x │ x │ o
 ───┼───┼───
- o │ x │ x 
+ o │ x │ x
 */
