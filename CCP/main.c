32670a4f27052fe1e870b1b47843804d354f02d6
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-10-20 20:53:02 +0500

    removed extra lib, simplified print_board func, corrected check_win func.

--- CCP/main.c
+++ CCP/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
-#include <stdlib.h>
 
 char board[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int win_pos[3];
 
-void print_board();
+void print_board(int status);
 int check_win();
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
-    int player = 1, box, flag = 0;
+    int player = 1, box, flag = 0, status = 0;
     char mark = 'X';
     while (1)
     {
-        print_board();
+        print_board(status);
         mark = player == 1 ? 'X' : 'O';
         if (flag)
             printf("Invalid input by Player %d\n\tEnter again.", player);
@@ -26,16 +26,16 @@ int main()
             continue;
         }
         board[box - 1] = mark;
-        int check = check_win();
-        if (check)
+        status = check_win();
+        if (status)
         {
-            print_board();
-            if (check == 1)
+            print_board(status);
+            if (status == 1)
             {
                 printf("Player %d Wins!!", player);
                 return 0;
             }
-            else if (check == -1)
+            else if (status == -1)
             {
                 printf("This is a draw...");
                 return 0;
@@ -47,17 +47,34 @@ int main()
     return 0;
 }
 
-void print_board()
+void print_board(int status)
 {
     system("cls");
     printf("\t┌────────────────┐\n");
     printf("\t│TIC TAC TOE GAME│\n");
     printf("\t└────────────────┘\n");
-    printf("\n\t    %c │ %c │ %c", board[0], board[1], board[2]);
-    printf("\n\t   ───┼───┼───");
-    printf("\n\t    %c │ %c │ %c", board[3], board[4], board[5]);
-    printf("\n\t   ───┼───┼───");
-    printf("\n\t    %c │ %c │ %c\n", board[6], board[7], board[8]);
+    for (int i = 0; i < 3; i++) {
+        printf("\n\t    ");
+        for (int j = 0; j < 3; j++) {
+            printf("%c", board[j+i*3]);
+            j!=2?printf(" │ "):0;
+        }
+        i!=2?printf("\n\t   ───┼───┼───"):0;
+    }
+    printf("\n");
+    /*
+    i j -> n
+    0 0 -> 0
+    0 1 -> 1
+    0 2 -> 2
+    1 0 -> 3
+    1 1 -> 4
+    1 2 -> 5
+    2 0 -> 6
+    2 1 -> 7
+    2 2 -> 8
+    n = j + i*3
+    */
 }
 
 int check_win()
@@ -73,8 +90,6 @@ int check_win()
         || board[2] == board[4] && board[4] == board[6] // lft dig
     )
         return 1;
-    else
-        return 0; // continue as normal
 
     // Draw Check
     int filled_box = 0;
@@ -85,6 +100,7 @@ int check_win()
     }
     if (filled_box == 9)
         return -1;
+    return 0; // continue as normal
 }
 
 /*
