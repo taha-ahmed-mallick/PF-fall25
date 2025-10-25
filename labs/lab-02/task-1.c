cb510939914ab68b4c56ff3d7aecd0dade52d27f
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-10-25 23:19:02 +0500

    required

--- labs/lab-02/task-1.c
+++ labs/lab-02/task-1.c
@@ -7,14 +7,7 @@ int main()
     scanf("%d", &age);
     if (age >= 18)
     {
-        int hasID;
         printf("You are eligible for Voting.");
-        printf("\nDo you have ID [0/1]: ");
-        scanf("%d", &hasID);
-        if (hasID)
-            printf("You can vote!!");
-        else
-            printf("Generate your ID to cast vote.");
     }
     else
     {
