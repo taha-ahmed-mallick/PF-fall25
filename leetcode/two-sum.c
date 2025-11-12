61f555442084fd4987711b25bd547455ade2bca3
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-12 23:30:30 +0500

    two sum problem done

---
+++ leetcode/two-sum.c
@@ -0,0 +1,27 @@
+/*
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+// Problem Link: https://leetcode.com/problems/two-sum/
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    *returnSize = 2;
+    int *result = (int *)malloc(2 * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return NULL; // No solution found
+}
