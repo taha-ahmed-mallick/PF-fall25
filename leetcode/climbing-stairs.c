eb9dca4d800365024942c4cb288d6a90ab4ec94d
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 00:44:34 +0500

    adding link

--- leetcode/climbing-stairs.c
+++ leetcode/climbing-stairs.c
@@ -1,11 +1,16 @@
-int climbStairs(int n) {
-    if (n <= 2) return n;
+// Problem Link: https://leetcode.com/problems/climbing-stairs/
+
+int climbStairs(int n)
+{
+    if (n <= 2)
+        return n;
 
     int prev2 = 1;
     int prev1 = 2;
     int current;
 
-    for (int i = 3; i <= n; i++) {
+    for (int i = 3; i <= n; i++)
+    {
         current = prev1 + prev2;
         prev2 = prev1;
         prev1 = current;
