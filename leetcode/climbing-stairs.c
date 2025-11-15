607f8d8f0c39be55fe7e15bdc41a9b2ff8ca1669
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-15 23:47:19 +0500

    lc 11th

---
+++ leetcode/climbing-stairs.c
@@ -0,0 +1,15 @@
+int climbStairs(int n) {
+    if (n <= 2) return n;
+
+    int prev2 = 1;
+    int prev1 = 2;
+    int current;
+
+    for (int i = 3; i <= n; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return current;
+}
