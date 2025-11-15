88f24e70f31990ed0ea57da8bd7b213164abfb9e
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 00:54:32 +0500

    lc 14th

---
+++ leetcode/single-number.c
@@ -0,0 +1,17 @@
+// Problem Link: https://leetcode.com/problems/single-number/
+
+int singleNumber(int *nums, int numsSize)
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < numsSize; j++)
+        {
+            if (nums[j] == nums[i])
+                count++;
+        }
+        if (count == 1)
+            return nums[i];
+    }
+    return 0;
+}
