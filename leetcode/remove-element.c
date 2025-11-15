c60164e664a2d0a64ca424ee11ac1559b45986bd
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 00:31:24 +0500

    a step towards <un>readability

--- leetcode/remove-element.c
+++ leetcode/remove-element.c
@@ -1,13 +1,12 @@
 // Problem Link: https://leetcode.com/problems/remove-element/
 
 int removeElement(int* nums, int numsSize, int val) {
-    for (int i = 0; i < numsSize; i++) {
+    for (int i = 0; i < numsSize; i++)
         if (val == *(nums+i)){
             for (int j = i+1; j < numsSize; j++)
                 *(nums+j-1) = *(nums+j);
             numsSize--;
             i--;
         }
-    }
     return numsSize;
 }
