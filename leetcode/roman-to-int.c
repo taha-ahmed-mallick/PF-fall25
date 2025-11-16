63272f21f0bbfe812d7284f60e766d7cf9523371
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 13:37:25 +0500

    beats 100%

--- leetcode/roman-to-int.c
+++ leetcode/roman-to-int.c
@@ -1,22 +1,25 @@
 // Problem Link: https://leetcode.com/problems/roman-to-integer/
 
+int val(char sym) {
+    switch(sym) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
 int romanToInt(char* s) {
-    int map[128] = {0};
-    map['I'] = 1;
-    map['V'] = 5;
-    map['X'] = 10;
-    map['L'] = 50;
-    map['C'] = 100;
-    map['D'] = 500;
-    map['M'] = 1000;
     int result = 0, current, next;
     for (int i = 0; s[i]!='\0'; i++) {
-        current = map[s[i]];
-        next = map[s[i+1]];
-        printf("%d ", current);
+        current = val(s[i]);
+        next = val(s[i+1]);
         if (current < next){
-            result += next - current;
-            i++;
+            result -= current;
         } else
             result += current;
     }
