64bccef55217a730679562165473ae249e18548e
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-17 19:19:49 +0500

    lc 22nd

---
+++ leetcode/plus-one.c
@@ -0,0 +1,29 @@
+// Problem Link: https://leetcode.com/problems/plus-one/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *plusOne(int *digits, int digitsSize, int *returnSize)
+{
+    digits[digitsSize - 1]++;
+    *returnSize = digitsSize;
+    for (int i = *returnSize - 1; i != 0; i--)
+        if (digits[i] == 10)
+        {
+            digits[i] = 0;
+            digits[i - 1]++;
+        }
+        else
+            break;
+    if (digits[0] == 10)
+    {
+        *(returnSize) += 1;
+        int *result = (int *)malloc(*returnSize * sizeof(int));
+        for (int i = *returnSize - 1; i != 1; i--)
+            result[i] = digits[i - 1];
+        result[0] = 1;
+        result[1] = 0;
+        return result;
+    }
+    return digits;
+}
