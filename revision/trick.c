abcc327ac029127357439feaecc33589ea68b567
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-25 17:48:49 +0500

    changes

--- revision/trick.c
+++ revision/trick.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 // print the numbers until users enters 0
 int main() {
-    for (int i = 2; i != 0; i--) {
+    for (int i = 1; i != 0; i--) {
         printf("%d\n", i);
         scanf("%d", &i);
         i++;
     }
+    printf("Terminating...\n");
     return 0;
 }
