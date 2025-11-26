99038793281181a35e3fa64c24f2e1ce44b5aaab
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-26 17:56:32 +0500

    added validation and made simplification conditions

--- revision/frac-add.c
+++ revision/frac-add.c
@@ -6,26 +6,47 @@ typedef struct
     int deno;
 } Frac;
 
-Frac input(int n) {
+Frac input(int n)
+{
     Frac frac;
     printf("====Fraction %d====\n", n);
     printf("Enter numerator: ");
     scanf("%d", &frac.nume);
     printf("Enter denominator: ");
     scanf("%d", &frac.deno);
+    // make it so that it checks for denominator != 0 and keeps asking until valid input is given
+    while (frac.deno == 0)
+    {
+        printf("Denominator cannot be zero. Please enter a valid denominator: ");
+        scanf("%d", &frac.deno);
+    }
     return frac;
 }
 
-Frac sum(Frac f1, Frac f2) {
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+Frac sum(Frac f1, Frac f2)
+{
     Frac frac;
-    frac.nume = f1.nume*f2.deno + f2.nume * f1.deno;
+    frac.nume = f1.nume * f2.deno + f2.nume * f1.deno;
     frac.deno = f1.deno * f2.deno;
+
+    int g = gcd(frac.nume, frac.deno);
+    frac.nume /= g;
+    frac.deno /= g;
+
     return frac;
 }
 
 // a/b + c/d = ad+bc / bd
 
-int main() {
+int main()
+{
     printf("\t=====Fraction Adder=====\n\n");
     Frac f1 = input(1);
     Frac f2 = input(2);
