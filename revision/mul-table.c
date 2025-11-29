2e8911124e2fe633b1cbe85399e98bfa2ba1c317
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-29 18:18:30 +0500

    idk 😪

--- revision/mul-table.c
+++ revision/mul-table.c
@@ -4,7 +4,12 @@
 #include <windows.h>
 #endif
 
-int main() {
+void printCell(int);
+int size(int);
+int max_digit = 0;
+
+int main()
+{
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
@@ -13,23 +18,112 @@ int main() {
     scanf("%d", &r);
     printf("Enter no. of columns: ");
     scanf("%d", &c);
-    int **tbl = (int **) malloc(r*sizeof(int *));
+    c++;
+    int **tbl = (int **)malloc(r * sizeof(int *));
     for (int i = 0; i < r; i++)
-        tbl[i] = (int*) malloc(c*sizeof(int));
-    for (int i = 0; i < r; i++){
-        for (int j = 0; j < c; j++){
-            tbl[i][j] = (i+1)*(j+1);
-            printf("%d", tbl[i][j]);
-        }
-        printf("\n");
-    }
-    for (int i = -1; i < r; i++){
-        for (int j = -1; j < c; j++){
-            if (i == -1) 
-            printf("%d", tbl[i][j]);
-        }
-        printf("\n");
+    {
+        tbl[i] = (int *)malloc(c * sizeof(int));
+        for (int j = 0; j < c; j++)
+            if (!j)
+                tbl[i][j] = (i + 1);
+            else
+                tbl[i][j] = (i + 1) * j;
     }
 
+    max_digit = size(tbl[r - 1][c - 1]);
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+    printf("\033[1;34m");
+    for (int i = -1; i < r; i++)
+    {
+        if (i == -1)
+        {
+            printf("┌");
+            for (int j = 0; j < c; j++)
+            {
+                for (int k = 0; k < max_digit; k++)
+                    printf("─");
+                if (j == c - 1)
+                    printf("┐\n");
+                else
+                    printf("┬");
+            }
+        }
+        for (int j = 0; j < c; j++)
+        {
+            printf("│");
+            if (i == -1)
+            {
+                if (j == 0)
+                {
+                    printCell(0);
+                }
+                else
+                {
+                    printCell(j);
+                }
+            }
+            else
+                printCell(tbl[i][j]);
+            if (j == c - 1)
+                printf("│\n");
+        }
+        if (i == r - 1)
+        {
+            printf("└");
+            for (int j = 0; j < c; j++)
+            {
+                for (int k = 0; k < max_digit; k++)
+                    printf("─");
+                if (j == c - 1)
+                    printf("┘\n");
+                else
+                    printf("┴");
+            }
+        }
+        else
+        {
+            printf("├");
+            for (int j = 0; j < c; j++)
+            {
+                for (int k = 0; k < max_digit; k++)
+                    printf("─");
+                if (j == c - 1)
+                    printf("┤\n");
+                else
+                    printf("┼");
+            }
+        }
+    }
+    for (int i = 0; i < r; i++)
+        free(tbl[i]);
+    free(tbl);
     return 0;
+}
+
+void printCell(int val)
+{
+    int cur_size = size(val);
+    if (val == 0)
+        printf("●");
+    else
+        printf("%d", val);
+    for (int i = 0; i < max_digit - cur_size; i++)
+        printf(" ");
+}
+
+int size(int val)
+{
+    int cur_size = 0;
+    if (val == 0)
+        return 1;
+    while (val != 0)
+    {
+        val /= 10;
+        cur_size++;
+    }
+    return cur_size;
 }
